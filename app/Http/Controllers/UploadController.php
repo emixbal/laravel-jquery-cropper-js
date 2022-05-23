a046193e55c5d5c55a94734533ed42343e0dc441
iqbal <emixbal@gmail.com>
2022-05-23 13:07:19 +0700

    fix upload tanpa foto

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -76,7 +76,6 @@ class UploadController extends Controller
 
     public function anggota_save(Request $request)
     {
-        $file_name = "";
         $folder_name = $request->input('folder_name');
         $nama   = $request->input('nama');
         $alamat = $request->input('alamat');
@@ -88,40 +87,43 @@ class UploadController extends Controller
         $dob = explode("/", $dob);
         $dob = $dob[2]."-".$dob[0]."-".$dob[1];
 
-        $image_base64 = $request->input('image_base64');
+        $image_base64 = $request->input('image_base64')??"";
+        $file_name = $request->input('file_name');
+
+        $path_sudah = session('path')."/sudah";
+        $path_ok = session('path')."/ok";
+        $is_photo_avail = 0;
+
+        if(is_dir($path_sudah)==FALSE){
+            mkdir($path_sudah);
+        }
+
+        if(is_dir($path_ok)==FALSE){
+            mkdir($path_ok);
+        }
+
         if($image_base64!=""){
-            $file_name = $request->input('file_name');
             $base_to_php = explode(',', $image_base64);
             $data = base64_decode($base_to_php[1]);
-    
-            $path_sudah = session('path')."/sudah";
-            $path_ok = session('path')."/ok";
-    
-            if(is_dir($path_sudah)==FALSE){
-                mkdir($path_sudah);
-            }
-    
-            if(is_dir($path_ok)==FALSE){
-                mkdir($path_ok);
-            }
-    
+
             // here you can detect if type is png or jpg if you want
             $filepath = $path_ok."/".$file_name; // or image.jpg
-    
+
             // Save the image in a defined path
             if(!file_put_contents($filepath, $data)){
                 echo "error";
                 return;
             }
-    
-            copy(session('path')."/".$file_name, $path_sudah."/".$file_name);
-            unlink(session('path')."/".$file_name);
+            $is_photo_avail = 1;
         }
 
-        $payload = [$nama, $pob, $dob, $alamat, $nik, $nip, session('path'), $folder_name, $file_name];
+        copy(session('path')."/".$file_name, $path_sudah."/".$file_name);
+        unlink(session('path')."/".$file_name);
+
+        $payload = [$nama, $pob, $dob, $alamat, $nik, $nip, session('path'), $folder_name, $file_name, $is_photo_avail];
         try {
             $results = DB::insert(
-                "insert into anggota (nama, pob, dob, alamat, nik, nip, path, folder_name, file_name) values (?,?,?,?,?,?,?,?,?)", $payload
+                "insert into anggota (nama, pob, dob, alamat, nik, nip, path, folder_name, file_name, is_photo_avail) values (?,?,?,?,?,?,?,?,?,?)", $payload
             );
         } catch(Exception $e) {
             print_r($e);
--- public/js/upload/cropper_custom.js
+++ public/js/upload/cropper_custom.js
@@ -1,119 +1,128 @@
-var $image = $('#image');
+$( document ).ready(function() {
+  var $image = $('#image');
 
-$image.cropper({
-  aspectRatio: 3/4,
-  crop: function (event) {
-    console.log(event.detail.x);
-    console.log(event.detail.y);
-    console.log(event.detail.width);
-    console.log(event.detail.height);
-    console.log(event.detail.rotate);
-    console.log(event.detail.scaleX);
-    console.log(event.detail.scaleY);
-  }
-});
-
-// Get the Cropper.js instance after initialized
-var cropper = $image.data('cropper');
-
-$("#crop").on("click", function () {
-  var canvas = $image.cropper('getCroppedCanvas');
-  $("#place_image").html();
-  $("#place_image").html(canvas);
-  return;
-})
-
-$("#plus").on("click", function () {
-  $image.cropper('rotate', +1);
-  return;
-})
-
-$("#minus").on("click", function () {
-  $image.cropper('rotate', -1);
-  return;
-})
-
-$('#simpan').on("click", function () {
-  var target = new Image();
-  target.src=""
-
-  var is_no_photo = ($('#is_no_photo').is(':checked'))?1:0
-  if(!is_no_photo){
-    var canvas = document.getElementsByTagName('canvas');
-    // DATAURL: Actual image generation via data url
-
-    if(typeof(canvas[0])=="undefined"){
-      alert("Crop foto terlebih dahulu")
-      return
-    }
-
-    target.src = canvas[0].toDataURL();
-  }
-
-  var file_name = ($('#file_name').val()) ? $('#file_name').val() : ""
-  var folder_name = ($('#folder_name').val()) ? $('#folder_name').val() : ""
-  var nama = ($('#nama').val()) ? $('#nama').val() : ""
-  var pob = ($('#pob').val()) ? $('#pob').val() : ""
-  var dob = ($('#dob').val()) ? $('#dob').val() : ""
-  var alamat = ($('#alamat').val()) ? $('#alamat').val() : ""
-  var nik = ($('#nik').val()) ? $('#nik').val() : ""
-  var nip = ($('#nip').val()) ? $('#nip').val() : ""
-
-  if (file_name == "") {
-    alert("file_name kosong")
-    return
-  }
-  
-  if (folder_name == "") {
-    alert("unit kerja kosong")
-    return
-  }
-
-  if (nama == "") {
-    alert("nama kosong")
-    return
-  }
-
-  if (pob == "") {
-    alert("tempat lahir kosong")
-    return
-  }
-  
-  if (dob == "") {
-    alert("tanggal lahir kosong")
-    return
-  }
-  
-  if (alamat == "") {
-    alert("alamat kosong")
-    return
-  }
-  
-  if (nik == "") {
-    alert("nik kosong")
-    return
-  }
-
-  if(is_no_photo){
-    confirm("Apakah anda yakin upload tanpa foto?"); 
-  }
-
-  $.ajax({
-    headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    },
-    url: "http://localhost:8000/anggota_save",
-    type: "POST",
-    data: {
-      file_name, folder_name, nama, pob, dob, alamat, nik, nip,
-      image_base64:target.src
-    },
-    success: function (response) {
-        console.log(response);
-        window.location.replace("http://localhost:8000/upload");
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-        console.log(textStatus, errorThrown);
+  $image.cropper({
+    aspectRatio: 3/4,
+    crop: function (event) {
+      console.log(event.detail.x);
+      console.log(event.detail.y);
+      console.log(event.detail.width);
+      console.log(event.detail.height);
+      console.log(event.detail.rotate);
+      console.log(event.detail.scaleX);
+      console.log(event.detail.scaleY);
     }
   });
+  
+  // Get the Cropper.js instance after initialized
+  var cropper = $image.data('cropper');
+  
+  $("#crop").on("click", function () {
+    var canvas = $image.cropper('getCroppedCanvas');
+    $("#place_image").html();
+    $("#place_image").html(canvas);
+    return;
+  })
+  
+  $("#plus").on("click", function () {
+    $image.cropper('rotate', +1);
+    return;
+  })
+  
+  $("#minus").on("click", function () {
+    $image.cropper('rotate', -1);
+    return;
+  })
+  
+  $('#simpan').on("click", function () {
+    var base64src = ""
+  
+    var is_no_photo = ($('#is_no_photo').is(':checked'))?1:0
+    if(!is_no_photo){
+      var canvas = document.getElementsByTagName('canvas');
+      // DATAURL: Actual image generation via data url
+  
+      if(typeof(canvas[0])=="undefined"){
+        alert("Crop foto terlebih dahulu")
+        return
+      }
+  
+      base64src = canvas[0].toDataURL();
+    }
+  
+    var file_name = ($('#file_name').val()) ? $('#file_name').val() : ""
+    var folder_name = ($('#folder_name').val()) ? $('#folder_name').val() : ""
+    var nama = ($('#nama').val()) ? $('#nama').val() : ""
+    var pob = ($('#pob').val()) ? $('#pob').val() : ""
+    var dob = ($('#dob').val()) ? $('#dob').val() : ""
+    var alamat = ($('#alamat').val()) ? $('#alamat').val() : ""
+    var nik = ($('#nik').val()) ? $('#nik').val() : ""
+    var nip = ($('#nip').val()) ? $('#nip').val() : ""
+  
+    if (file_name == "") {
+      alert("file_name kosong")
+      return
+    }
+    
+    if (folder_name == "") {
+      alert("unit kerja kosong")
+      return
+    }
+  
+    if (nama == "") {
+      alert("nama kosong")
+      return
+    }
+  
+    if (pob == "") {
+      alert("tempat lahir kosong")
+      return
+    }
+    
+    if (dob == "") {
+      alert("tanggal lahir kosong")
+      return
+    }
+    
+    if (alamat == "") {
+      alert("alamat kosong")
+      return
+    }
+    
+    if (nik == "") {
+      alert("nik kosong")
+      return
+    }
+  
+    if(is_no_photo){
+      var confirm_txt = "Apakah anda yakin upload tanpa foto?"
+      if (confirm(confirm_txt) == true) {
+        simpan()
+        return
+      }
+    }
+    simpan()
+  
+    function simpan(){
+      $.ajax({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        url: "http://localhost:8000/anggota_save",
+        type: "POST",
+        data: {
+          file_name, folder_name, nama, pob, dob, alamat, nik, nip,
+          image_base64:base64src
+        },
+        success: function (response) {
+            console.log(response);
+            window.location.replace("http://localhost:8000/upload");
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+        }
+      });
+      return;
+    }
+  })
 })
